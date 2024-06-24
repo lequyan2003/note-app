66294724646bc0e67f496e95207b47f1dceadbd4
lequyan2003 <lequyan2003@gmail.com>
2024-06-23 22:53:48 -0400

    ready to deploy

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,2 +1,2 @@
-export const GRAPHQL_SERVER = 'http://localhost:4000';
-export const GRAPHQL_SUBSCRIPTION_ENDPOINT = 'ws://localhost:4000/graphql';
+export const GRAPHQL_SERVER = import.meta.env.VITE_GRAPHQL_SERVER;
+export const GRAPHQL_SUBSCRIPTION_ENDPOINT = import.meta.env.VITE_GRAPHQL_SUBSCRIPTION_ENDPOINT;
